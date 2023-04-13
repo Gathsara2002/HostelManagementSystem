afa0addcb0036771cba1797f46237e54212ece03
Gathsara2002 <liviru2002mmg@gmail.com>
2023-04-13 10:43:02 +0530

    Added factory configuration

---
+++ src/lk/ijse/hostel/pos/util/SessionFactoryConfiguration.java
@@ -0,0 +1,32 @@
+/**
+ * @author : Gathsara
+ * created : 4/13/2023 -- 10:34 AM
+ **/
+
+package lk.ijse.hostel.pos.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryConfiguration {
+
+    private static SessionFactoryConfiguration sessionFactoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private SessionFactoryConfiguration() {
+        Configuration configuration = new Configuration().configure().addAnnotatedClass();
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static SessionFactoryConfiguration getInstance() {
+        return (sessionFactoryConfiguration == null) ?
+                sessionFactoryConfiguration = new SessionFactoryConfiguration()
+                : sessionFactoryConfiguration;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+}
