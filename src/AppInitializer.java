79294cb087954d56e17631ded0f2ee00de8f791d
Gathsara2002 <liviru2002mmg@gmail.com>
2023-04-13 10:11:31 +0530

    Project added

---
+++ src/AppInitializer.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author : Gathsara
+ * created : 4/13/2023 -- 10:04 AM
+ **/
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/lk/ijse/hostel/pos/controller/LoginForm.java
@@ -0,0 +1,9 @@
+/**
+ * @author : Gathsara
+ * created : 4/13/2023 -- 10:08 AM
+ **/
+
+package lk.ijse.hostel.pos.controller;
+
+public class LoginForm {
+}
