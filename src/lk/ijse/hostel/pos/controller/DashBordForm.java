ea32450243b7986d5d3d2821e320469364e3c3e5
Gathsara2002 <liviru2002mmg@gmail.com>
2023-04-13 16:43:54 +0530

    created admin dashboard ui

---
+++ src/lk/ijse/hostel/pos/controller/DashBordForm.java
@@ -0,0 +1,36 @@
+/**
+ * @author : Gathsara
+ * created : 4/13/2023 -- 12:12 PM
+ **/
+
+package lk.ijse.hostel.pos.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
+public class DashBordForm {
+    public AnchorPane dashBoardAP;
+    public Button btnUser;
+    public Button btnReserve;
+    public Button btnRoom;
+    public Button btnStudent;
+    public Circle btnHome;
+
+    public void manageUserOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReserveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnageRoomOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStdOnAction(ActionEvent actionEvent) {
+    }
+
+    public void homeOnAction(MouseEvent mouseEvent) {
+    }
+}
--- src/lk/ijse/hostel/pos/controller/LoginForm.java
+++ src/lk/ijse/hostel/pos/controller/LoginForm.java
@@ -7,15 +7,35 @@ package lk.ijse.hostel.pos.controller;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class LoginForm {
+
     public AnchorPane loginAP;
     public JFXButton btnLogin;
     public TextField txtUsername;
     public TextField txtPassword;
 
+//    String passWord=txtPassword.getText();
+//    String userName=txtUsername.getText();
+
     public void logInOnAction(ActionEvent actionEvent) {
+
+        try {
+            URL resource = getClass().getResource("/lk/ijse/hostel/pos/view/DashBordForm.fxml");
+            Parent load = FXMLLoader.load(resource);
+            Stage window = (Stage) loginAP.getScene().getWindow();
+            window.setScene(new Scene(load));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
