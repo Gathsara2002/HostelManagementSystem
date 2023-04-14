26f75660dd06de2e63de1fda950ab1cf3f20d22d
Gathsara2002 <liviru2002mmg@gmail.com>
2023-04-14 14:05:08 +0530

    create user form

---
+++ src/lk/ijse/hostel/pos/controller/UserFormController.java
@@ -0,0 +1,44 @@
+/**
+ * @author : Gathsara
+ * created : 4/14/2023 -- 12:23 PM
+ **/
+
+package lk.ijse.hostel.pos.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
+public class UserFormController {
+    public AnchorPane userAP;
+    public Circle btnHome;
+    public ImageView btnBack;
+    public PasswordField pfPassword;
+    public JFXButton btnUpdate;
+    public ImageView btnshowPassword;
+    public ImageView btnHidePw;
+    public TextField txtUsername;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtContact;
+
+    public void homeOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void backOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void showPwOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void hidePwOnAction(MouseEvent mouseEvent) {
+    }
+}
