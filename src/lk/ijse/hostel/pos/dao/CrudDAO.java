835b9443f6da6b3d3fa8b9a6af19b8cd0397bb13
Gathsara2002 <liviru2002mmg@gmail.com>
2023-04-15 14:25:57 +0530

    added crudDao and superDao to project

---
+++ src/lk/ijse/hostel/pos/dao/CrudDAO.java
@@ -0,0 +1,25 @@
+/**
+ * @author : Gathsara
+ * created : 4/14/2023 -- 8:06 PM
+ **/
+
+package lk.ijse.hostel.pos.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CrudDAO<T ,ID> extends SuperDAO{
+
+    ArrayList<T> getAll() throws SQLException, ClassNotFoundException;
+
+    boolean save(T t) throws SQLException, ClassNotFoundException;
+
+    boolean update(T t) throws SQLException, ClassNotFoundException;
+
+    boolean delete(ID id) throws SQLException, ClassNotFoundException;
+
+    T search(ID id) throws SQLException, ClassNotFoundException;
+
+    boolean exist(ID id) throws SQLException, ClassNotFoundException;
+
+}
---
+++ src/lk/ijse/hostel/pos/dao/SuperDAO.java
@@ -0,0 +1,9 @@
+/**
+ * @author : Gathsara
+ * created : 4/14/2023 -- 8:06 PM
+ **/
+
+package lk.ijse.hostel.pos.dao;
+
+public interface SuperDAO {
+}
