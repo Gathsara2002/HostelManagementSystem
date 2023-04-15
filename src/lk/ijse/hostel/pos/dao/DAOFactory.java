f788e7ddb8699a6f69360e58bd31be817ca249d7
Gathsara2002 <liviru2002mmg@gmail.com>
2023-04-15 15:41:36 +0530

    added DAOFactory

---
+++ src/lk/ijse/hostel/pos/dao/DAOFactory.java
@@ -0,0 +1,39 @@
+/**
+ * @author : Gathsara
+ * created : 4/15/2023 -- 3:23 PM
+ **/
+
+package lk.ijse.hostel.pos.dao;
+
+import lk.ijse.hostel.pos.dao.custom.impl.*;
+
+public class DAOFactory {
+    private static DAOFactory daoFactory;
+
+    private DAOFactory() {
+    }
+
+    public static DAOFactory getDAOFactory() {
+        return daoFactory == null ? daoFactory = new DAOFactory() : daoFactory;
+    }
+
+    public SuperDAO getDAO(DAOTypes types) {
+        switch (types) {
+            case STUDENT:
+                return new StudentDAOImpl();
+            case ROOM:
+                return new RoomDAOImpl();
+            case RESERVATION:
+                return new ReservationDAOImpl();
+            case USER:
+                return new UserDAOImpl();
+            default:
+                return null;
+        }
+    }
+
+    public enum DAOTypes {
+        ROOM, STUDENT, RESERVATION, USER
+    }
+
+}
